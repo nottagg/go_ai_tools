7cfce425cdadeb710da4073260ac11f67ff6e00f
nottagg <congdonjc@gmail.com>
2025-04-10 19:00:33 -0400

    Scaffolding grid

--- search/grid.go
+++ search/grid.go
@@ -1,6 +1,7 @@
 package search
 
 import (
+	"fmt"
 	"math/rand"
 )
 
@@ -15,9 +16,14 @@ type Grid struct {
 type Cell struct {
 	X       int
 	Y       int
+	isBlock bool
 	Weight  int
 	Visited bool
 	Parent  *Cell
+}
+
+type SearchType struct {
+	SearchType string
 }
 
 // NewGrid creates a new grid with the specified width and height.
@@ -27,7 +33,7 @@ func NewGrid(width, height, max_weight int, random bool) *Grid {
 	cells := make([][]*Cell, height)
 	for y := 0; y < height; y++ {
 		cells[y] = make([]*Cell, width)
-		for x := 0; x < width; x++ {
+		for x := range width {
 			weight := 1
 			if random {
 				weight = rand.Intn(max_weight) // Random weight between 0 and max_weight
@@ -51,6 +57,14 @@ func (g *Grid) GetCell(x, y int) *Cell {
 	return g.Cells[y][x]
 }
 
+// SetBlock sets the cell at the specified coordinates as a block.
+func (g *Grid) SetBlock(x, y int) {
+	cell := g.GetCell(x, y)
+	if cell != nil {
+		cell.isBlock = true
+	}
+}
+
 // SetWeight sets the weight of the cell at the specified coordinates.
 func (g *Grid) SetWeight(x, y, weight int) {
 	cell := g.GetCell(x, y)
@@ -60,22 +74,54 @@ func (g *Grid) SetWeight(x, y, weight int) {
 }
 
 // GetNeighbors returns the neighboring cells of the specified cell.
-func (g *Grid) GetNeighbors(cell *Cell) []*Cell {
+// If allowDiagonal is true, diagonal neighbors are included.
+// Otherwise, only orthogonal neighbors are included.
+func (g *Grid) GetNeighbors(cell *Cell, allowDiagonal bool) []*Cell {
 	neighbors := []*Cell{}
-	directions := []struct {
+	var directions []struct {
 		dx int
 		dy int
-	}{
-		{-1, 0}, // left
-		{1, 0},  // right
-		{0, -1}, // up
-		{0, 1},  // down
+	}
+	if allowDiagonal {
+		directions = []struct {
+			dx int
+			dy int
+		}{
+			{-1, -1}, {-1, 0}, {-1, 1},
+			{0, -1}, {0, 1},
+			{1, -1}, {1, 0}, {1, 1},
+		}
+	} else {
+		directions = []struct {
+			dx int
+			dy int
+		}{
+			{-1, 0},
+			{0, -1},
+			{0, 1},
+			{1, 0},
+		}
 	}
 	for _, dir := range directions {
 		nx, ny := cell.X+dir.dx, cell.Y+dir.dy
-		if neighbor := g.GetCell(nx, ny); neighbor != nil {
+		if neighbor := g.GetCell(nx, ny); neighbor != nil && !neighbor.isBlock {
 			neighbors = append(neighbors, neighbor)
 		}
 	}
 	return neighbors
+}
+
+func (g *Grid) ExecuteSearch(startCell, endCell *Cell, searchType SearchType) ([]*Cell, error) {
+	switch searchType.SearchType {
+	case "BFS":
+		return g.BFS(startCell, endCell)
+	case "DFS":
+		return g.DFS(startCell, endCell)
+	case "Dijkstra":
+		return g.Dijkstra(startCell, endCell)
+	case "AStar":
+		return g.AStar(startCell, endCell)
+	default:
+		return nil, fmt.Errorf("unsupported search type: %s", searchType.SearchType)
+	}
 }
